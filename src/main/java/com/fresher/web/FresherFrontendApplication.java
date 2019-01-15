3c6c4d111c8d3cef9f8f6cceb9da448654406f60
Bui Van Thuong (FA.G0) <ThuongBV@fsoft.com.vn>
2019-01-15 16:18:47 +0700

    add api up image

--- src/main/java/com/fresher/web/FresherFrontendApplication.java
+++ src/main/java/com/fresher/web/FresherFrontendApplication.java
@@ -2,8 +2,12 @@ package com.fresher.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+    FileStorageProperties.class
+})
 public class FresherFrontendApplication {
 
 	public static void main(String[] args) {
